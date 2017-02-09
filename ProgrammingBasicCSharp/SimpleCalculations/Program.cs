d3ca8719edad70ea3411edca93b293eb516e5eca
ivaylo95 <ivaylo_k_o@abv.bg>
2017-02-09 09:54:58 +0200

    1000 Days After Birth

--- ProgrammingBasicCSharp/SimpleCalculations/Program.cs
+++ ProgrammingBasicCSharp/SimpleCalculations/Program.cs
@@ -4,26 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2D_Rectangle_Area
+namespace _1000_Days_After_Birth
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("x1: ");
-            var x1 = double.Parse(Console.ReadLine());
-            Console.Write("y1: ");
-            var y1 = double.Parse(Console.ReadLine());
-            Console.Write("x2: ");
-            var x2 = double.Parse(Console.ReadLine());
-            Console.Write("y2: ");
-            var y2 = double.Parse(Console.ReadLine());
-            var a = Math.Abs(x1 - x2);
-            var b = Math.Abs(y1 - y2);
-            var area = a * b;
-            var perimeter = 2 * (a + b);
-            Console.WriteLine("Rectangle area is: {0}", area);
-            Console.WriteLine("Rectangle perimetr is: {0}", perimeter);
+            Console.Write("Input your birthday: ");
+            string format = "dd-MM-yyyy";
+            string value = Console.ReadLine();
+            DateTime userBirthday = DateTime.ParseExact(value, format, null);
+            Console.WriteLine(userBirthday.AddDays(999).ToString(format));
             Console.ReadLine();
         }
     }

8ab2f97b815117e51c9ad369e854e2c723d9a799
ivaylo95 <ivaylo_k_o@abv.bg>
2017-02-09 11:10:17 +0200

    Numbers 1...N with Step 3

--- Advanced-Loops/Numbers 1...N with Step 3/Program.cs
+++ Advanced-Loops/Numbers 1...N with Step 3/Program.cs
@@ -10,6 +10,12 @@ namespace Numbers_1._._.N_with_Step_3
     {
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }

a5dff46654931bbbed82e61ac26c2c8d459216c8
ivaylo95 <ivaylo_k_o@abv.bg>
2017-02-09 10:00:35 +0200

    Circle Area and Perimeter

--- ProgrammingBasicCSharp/SimpleCalculations/Program.cs
+++ ProgrammingBasicCSharp/SimpleCalculations/Program.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Celsius_to_Fahrenheit
+namespace Circle_Area_and_Perimeter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("C: ");
-            var C = double.Parse(Console.ReadLine());
-            var F = C * 1.8 + 32;
-            Console.WriteLine("F: {0}",Math.Round(F,2));
+            Console.Write("Input radius: ");
+            var r = double.Parse(Console.ReadLine());
+            var CirclePerimeter = 2 * Math.PI * r;
+            var CircleArea = Math.PI * r * r;
+            Console.WriteLine("Circle perimeter is: {0} and circle area is: {1}",CirclePerimeter,CircleArea);
             Console.ReadLine();
         }
     }
